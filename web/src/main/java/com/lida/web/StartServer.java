c99b642540fe09b4a6d3648d8be5e551762a24a2
join_wick <854403361@qq.com>
2021-03-06 23:46:20 +0800

    initial project

---
+++ web/src/main/java/com/lida/web/StartServer.java
@@ -0,0 +1,18 @@
+package com.lida.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author join wick
+ * @version 1.0.0
+ * @description 启动服务
+ * @createDate 2021/3/6 23:13
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class StartServer {
+    public static void main(String[] args) {
+        SpringApplication.run(StartServer.class);
+    }
+}
---
+++ web/src/main/java/com/lida/web/controller/DemoController.java
@@ -0,0 +1,30 @@
+package com.lida.web.controller;
+
+import com.lida.entity.DemoEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author join wick
+ * @version 1.0.0
+ * @description demo controller
+ * @createDate 2021/3/6 23:13
+ * @since 1.0.0
+ */
+@Controller
+public class DemoController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DemoController.class);
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public DemoEntity test() {
+        LOGGER.info("用户访问 test api");
+        DemoEntity demoEntity = new DemoEntity();
+        demoEntity.setName("小强");
+        demoEntity.setAge(24);
+        return demoEntity;
+    }
+}
